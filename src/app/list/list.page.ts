5cce8d226eea2425035602c670771dcb3902b3af
Arianne Coyco <yhangel.07@icloud.com>
2019-08-25 15:17:28 +0800

    user current location

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,6 +1,9 @@
 
-import { Component, OnInit } from '@angular/core';
-import { latLng, LatLng, tileLayer } from 'leaflet';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { icon, latLng, marker, polyline, tileLayer } from 'leaflet';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NgStyle } from '@angular/common';
+import { NullAstVisitor } from '@angular/compiler';
 
 
 @Component({
@@ -9,25 +12,71 @@ import { latLng, LatLng, tileLayer } from 'leaflet';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit{
-  ngOnInit(){
-    
+
+  mapInit: Boolean = false;
+  center: any = null;
+  layers: any = null;
+  userLoc: any;
+
+  ngOnInit(){ 
+    this.userCurrentLocation();
   }
-  
+
+  constructor(private zone: NgZone, private geolocation: Geolocation) {}
+
+  streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    maxZoom: 18,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+ 
+
   options = {
-    layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909)
+    layers: [this.streetMaps],
+    zoom: 17,
+    center: this.userCurrentLocation()
   };
+
+  userCurrentLocation(){
+
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+      this.center = latLng(resp.coords.latitude, resp.coords.longitude);
+      
+      this.userLoc =  marker([ resp.coords.latitude, resp.coords.longitude ], {
+        icon: icon({
+          iconSize: [ 25, 41 ],
+          iconAnchor: [ 13, 41 ],
+          iconUrl: '../assets/img/default_location_marker.png',
+          shadowUrl: 'assets/marker-shadow.png'
+        })
+      });
+
+      this.layers = [this.streetMaps, this.userLoc];
+
+      this.mapInit = true;
+      //console.log(resp.coords.latitude, resp.coords.longitude);
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+  }
+
+  // this.mapOptions = {
+  //   layers: [
+  //     tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+  //   ],
+  //   zoom: 17,
+  //   center: latLng(this.lat, this.long)
+  // };
 
   onMapReady(map: L.Map) {
     setTimeout(() => {
       map.invalidateSize();
     }, 0);
   }
-
-
 }
 
 
